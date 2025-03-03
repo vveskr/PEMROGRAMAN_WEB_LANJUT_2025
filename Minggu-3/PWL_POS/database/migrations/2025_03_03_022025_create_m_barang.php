5a136485f66682dea71b89d6299e8b6ba71ec3ef
Lovelyta Sekarayu Krisdiyanti <vveskr@gmail.com>
2025-03-03 09:38:39 +0700

    update praktikum 2.2

---
+++ Minggu-3/PWL_POS/database/migrations/2025_03_03_022025_create_m_barang.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('m_barang', function (Blueprint $table) {
+            $table->id('barang_id');
+            $table->unsignedBigInteger('kategori_id')->index;
+            $table->string('barang_kode', 10)->unique();
+            $table->string('barang_nama', 100);
+            $table->integer('harga_beli');
+            $table->integer('harga_jual');
+            $table->timestamps();
+
+            //menambah foreign key constrait
+            $table->foreign('kategori_id')->references('id')->on('m_kategori')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('m_barang');
+    }
+};
