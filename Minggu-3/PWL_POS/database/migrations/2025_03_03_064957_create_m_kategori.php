f8e9f8c4e69dc297936a1e63d4c3dae99ffbdaed
Lovelyta Sekarayu Krisdiyanti <vveskr@gmail.com>
2025-03-03 13:51:59 +0700

    update praktikum 2.1

--- Minggu-3/PWL_POS/database/migrations/2025_03_03_064957_create_m_kategori.php
+++ Minggu-3/PWL_POS/database/migrations/2025_03_03_064957_create_m_kategori.php
@@ -11,11 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('m_level', function (Blueprint $table) {
-            $table->id('level_id');
-            $table->string('level_kode', 10)->unique();
-            $table->string('level_nama', 100);
-            $table->timestamps();
+        Schema::create('m_kategori', function (Blueprint $table) {
+            $table->unsignedBigInteger('kategori_id', 20);
+            $table->string('kategori_kode', 10);
+            $table->string('kategori_nama', 100);
+            $table->timestamps(); 
         });
     }
 
@@ -24,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('m_level');
+        Schema::dropIfExists('m_kategori');
     }
 };
