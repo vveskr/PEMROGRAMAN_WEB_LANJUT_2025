8b3ce213f51c92be6e0c4bae9642ca38d0abc6d3
Lovelyta Sekarayu Krisdiyanti <vveskr@gmail.com>
2025-03-03 17:03:28 +0700

    update Praktikum 6

--- Minggu-3/PWL_POS/routes/web.php
+++ Minggu-3/PWL_POS/routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 
 
@@ -12,3 +13,4 @@ Route::get('/', function () {
 
 Route::get('/level', [LevelController::class, 'index']);
 Route::get('/kategori', [KategoriController::class, 'index']); 
+Route::get('/user', [UserController::class, 'index']);
