eed5c65527c1d1227156746a80acf685e7299dd9
Lovelyta Sekarayu Krisdiyanti <vveskr@gmail.com>
2025-03-09 10:28:09 +0700

    Jobsheet 4

---
+++ Minggu-4/PWL_POS/routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\LevelController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/level', [LevelController::class, 'index']);
+Route::get('/kategori', [KategoriController::class, 'index']); 
+Route::get('/user', [UserController::class, 'index']);
