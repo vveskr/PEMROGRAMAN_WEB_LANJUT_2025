9b727f6d8d99b0d40abd4120687f1bd4d0b97d36
Lovelyta Sekarayu Krisdiyanti <vveskr@gmail.com>
2025-03-10 13:01:16 +0700

    Update Praktikum 2.7

--- Minggu-4/PWL_POS/app/Models/UserModel.php
+++ Minggu-4/PWL_POS/app/Models/UserModel.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use App\Models\LevelModel;
 
 class UserModel extends Model
 {
@@ -13,4 +15,9 @@ class UserModel extends Model
     protected $primaryKey = 'user_id';
 
     protected $fillable = ['level_id', 'username', 'nama', 'password'];
+
+    public function level(): BelongsTo
+    {
+        return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+    }
 }
--- Minggu-4/PWL_POS/resources/views/user.blade.php
+++ Minggu-4/PWL_POS/resources/views/user.blade.php
@@ -12,6 +12,8 @@
             <td>Username</td>
             <td>Nama</td>
             <td>ID Level Pengguna</td>
+            <td>Kode Level</td>
+            <td>Nama Level</td>
             <td>Aksi</td>
         </tr>
         @foreach($data as $d)
@@ -20,6 +22,8 @@
             <td>{{ $d->username }}</td>
             <td>{{ $d->nama }}</td>
             <td>{{ $d->level_id }}</td>
+            <td>{{ $d->level->level_kode }}</td>
+            <td>{{ $d->level->level_name }}</td>
             <td><a href="http://localhost/PEMROGRAMAN_WEB_LANJUT_2025/Minggu-4/PWL_POS/public/user/ubah/{{ $d->user_id }}">Ubah</a> | <a href="http://localhost/PEMROGRAMAN_WEB_LANJUT_2025/Minggu-4/PWL_POS/public/user/hapus/{{ $d->user_id }}">Hapus</a></td>
         <tr/>
         @endforeach
