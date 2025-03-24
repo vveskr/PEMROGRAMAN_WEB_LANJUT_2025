c446be4cacd206ba11d35abdb4b2439124a437fd
Lovelyta Sekarayu Krisdiyanti <vveskr@gmail.com>
2025-03-24 11:44:48 +0700

    Praktikum 2

--- Minggu-5/PWL_POS/routes/web.php
+++ Minggu-5/PWL_POS/routes/web.php
@@ -4,8 +4,7 @@ use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
-
-
+use App\Http\Controllers\WelcomeController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -22,3 +21,6 @@ Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
 Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
 
 Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
+
+Route::get('/', [WelcomeController::class, 'index']);
+
