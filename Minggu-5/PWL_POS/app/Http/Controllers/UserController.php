d56b7dc4a41f8d8ea749636214900c2617439298
Lovelyta Sekarayu Krisdiyanti <vveskr@gmail.com>
2025-03-24 22:32:20 +0700

    Praktikum 4

--- Minggu-5/PWL_POS/app/Http/Controllers/UserController.php
+++ Minggu-5/PWL_POS/app/Http/Controllers/UserController.php
@@ -24,7 +24,9 @@ class UserController extends Controller
             
             $activeMenu = 'user'; // set menu yang sedang aktif
             
-            return view('user.index', ['breadcrumb' => $breadcrumb, 'page' => $page, 'activeMenu' => $activeMenu]);
+            $level = LevelModel::all(); // ambil data level untuk filter level
+
+            return view('user.index', ['breadcrumb' => $breadcrumb, 'page' => $page, 'level' => $level, 'activeMenu' => $activeMenu]);
     }
 
     // Ambil data user dalam bentuk json untuk datatables 
@@ -32,6 +34,11 @@ class UserController extends Controller
     { 
         $users = UserModel::select('user_id', 'username', 'nama', 'level_id') ->with('level'); 
  
+        // Filter data user berdasarkan level_id
+        if ($request->level_id) {
+            $users->where('level_id', $request->level_id);
+        }
+        
         return DataTables::of($users) 
         // menambahkan kolom index / no urut (default nama kolom: DT_RowIndex) 
         ->addIndexColumn()  
