ca93a840215445edf80e4552bf6d4865f3c3ca7f
Lovelyta Sekarayu Krisdiyanti <vveskr@gmail.com>
2025-04-10 20:41:26 +0700

    implementasi JS 4 (6)

--- POS/app/Http/Controllers/UserController.php
+++ POS/app/Http/Controllers/UserController.php
@@ -10,23 +10,47 @@ class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::create([
-            'username' => 'manager11',
-            'nama' => 'Manager11',
-            'password' => Hash::make('12345'),
-            'level_id' => 2,
+        $user = UserModel::all();
+        return view(view: 'user', data: ['data' => $user]);
+    }
+
+    public function tambah()
+    {
+        return view(view: 'user_tambah');
+    }
+
+    public function tambah_simpan(Request $request)
+    {
+        UserModel::create([
+            'username' => $request->username,
+            'nama' => $request->nama,
+            'password' => Hash::make($request->password),
+            'level_id' => $request->level_id,
         ]);
 
-        $user->username = 'manager12';
+        return redirect(to: '/user');
+    }
+    public function ubah($id)
+    {
+        $user = UserModel::find($id);
+        return view('user_ubah', ['data' => $user]);
+    }
 
+    public function ubah_simpan($id, Request $request)
+    {
+        $user = UserModel::find($id);
+        $user->username = $request->username;
+        $user->nama = $request->nama;
+        $user->password = Hash::make($request->password);
+        $user->level_id = $request->level_id;
         $user->save();
+        return redirect('/user');
+    }
+    public function hapus($id)
+    {
+        $user = UserModel::find($id);
+        $user->delete();
 
-        $user->wasChanged(); // true
-        $user->wasChanged('username'); // true
-        $user->wasChanged(['username', 'level_id']); // true
-        $user->wasChanged('nama'); // false
-        dd(vars: $user->wasChanged(['nama', 'username'])); // true
-
-        return view(view: 'user', data: ['data' => $user]);
+        return redirect('/user');
     }
 }
