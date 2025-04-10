c0b2c03250eae20c94ceb56481044229f5a058be
Lovelyta Sekarayu Krisdiyanti <vveskr@gmail.com>
2025-04-10 20:36:48 +0700

    implementasi JS 4 (5)

--- POS/app/Http/Controllers/UserController.php
+++ POS/app/Http/Controllers/UserController.php
@@ -10,17 +10,23 @@ class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::firstOrNew(
-            [
-                'username' => 'manager33',
-                'nama' => 'Manager Tiga Tiga',
-                'password' => Hash::make('12345'),
-                'level_id' => 2
-            ]
-        );
+        $user = UserModel::create([
+            'username' => 'manager11',
+            'nama' => 'Manager11',
+            'password' => Hash::make('12345'),
+            'level_id' => 2,
+        ]);
+
+        $user->username = 'manager12';
 
         $user->save();
 
-        return view('user', ['data' => $user]);
+        $user->wasChanged(); // true
+        $user->wasChanged('username'); // true
+        $user->wasChanged(['username', 'level_id']); // true
+        $user->wasChanged('nama'); // false
+        dd(vars: $user->wasChanged(['nama', 'username'])); // true
+
+        return view(view: 'user', data: ['data' => $user]);
     }
 }
